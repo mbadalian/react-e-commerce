44ce28c08196c3268a388789267a50f8fb2673e9
vladislav.fomin <vladislav.fomin@aliexpress.ru>
2023-07-06 21:06:41 +0300

    fix: tabs structure

--- src/ui/Tabs/index.tsx
+++ src/ui/Tabs/index.tsx
@@ -1,41 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, ReactElement, ReactNode } from 'react';
 
 import "./index.css";
 
-interface Tab {
-  title: string;
-  content: React.ReactNode;
+export interface TabProps {
+    id: string;
+    title: string;
+    active?: boolean;
+    onClick?: () => void;
+    disabled?: boolean;
+    children: ReactNode;
 }
 
 interface TabsProps {
-  tabs: Tab[];
+  children: ReactNode;
 }
 
-export const Tabs: React.FC<TabsProps> = ({ tabs }) => {
-  const [activeTab, setActiveTab] = useState(0);
+export const Tab: React.FC<TabProps> = ({ title, active, onClick }) => {
+    return (
+        <div className={`tab-title ${active ? "active" : ""}`} onClick={onClick}>
+           {title}
+        </div>
+    );
+};
+
+export const Tabs: React.FC<TabsProps> = ({ children }) => {
+  const childrenArray = React.Children.toArray(children) as React.ReactElement[];
+  const [activeTab, setActiveTab] = useState(childrenArray[0].props.id);
 
   return (
     <div className="tabs">
       <div className="tab-titles">
-        {tabs.map((tab, index) => (
-          <div
-            key={index}
-            className={`tab-title ${activeTab === index ? "active" : ""}`}
-            onClick={() => setActiveTab(index)}
-          >
-            {tab.title}
-          </div>
-        ))}
+          {childrenArray.map((child: ReactElement<TabProps>) => {
+              const { id } = child.props;
+              const active = id === activeTab;
+
+              return (
+                  React.cloneElement(child, {
+                      active,
+                      onClick: () => setActiveTab(id),
+                  })
+              );
+          })}
       </div>
       <div className="tab-contents">
-        {tabs.map((tab, index) => (
-          <div
-            key={index}
-            className={`tab-content ${activeTab === index ? "active" : ""}`}
-          >
-            {tab.content}
-          </div>
-        ))}
+          {childrenArray.map((child: ReactElement<TabProps>) => {
+              const { children, id, title } = child.props;
+
+              if (!children) return null;
+
+              return <div
+                  key={id + title}
+                  className={`tab-content ${activeTab === id ? "active" : ""}`}
+              >
+                  {children}
+              </div>
+          })}
       </div>
     </div>
   );
