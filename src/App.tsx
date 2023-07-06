d53458acfcbf5fe3e7d3a4198a2949c61ad2ae60
mbadalian <supreme.opp@gmail.com>
2023-07-06 18:23:17 +0300

    add Tabs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
 import React from "react";
 
-import { Button, Card } from "./ui/";
+import { Card, Tabs } from "./ui/";
 
 import "./App.css";
 
 function App() {
+  interface Product {
+    id: number;
+    image: string;
+    title: string;
+    description: string;
+    price: number;
+  }
+
+  const products: Product[] = [
+    {
+      id: 1,
+      image: "mock-2.png",
+      title: "Product 1",
+      description: "This is the description for Product 1.",
+      price: 9.99,
+    },
+    {
+      id: 2,
+      image: "mock.png",
+      title: "Product 2",
+      description: "This is the description for Product 2.",
+      price: 19.99,
+    },
+    {
+      id: 3,
+      image: "https://via.placeholder.com/150",
+      title: "Product 3",
+      description: "This is the description for Product 3.",
+      price: 29.99,
+    },
+  ];
+
+  const tabs: { title: string; products: Product[] }[] = [
+    {
+      title: "Gadgets",
+      products: products.slice(0, 2),
+    },
+    {
+      title: "Fitness",
+      products: products.slice(1, 3),
+    },
+    {
+      title: "Education",
+      products: products.slice(0, 3),
+    },
+  ];
+
   return (
     <>
-      <Button
-        variant="contained"
-        size="large"
-        label="Learn More"
-        onClick={() => {}}
+      <Tabs
+        tabs={tabs.map((tab) => ({
+          title: tab.title,
+          content: (
+            <div className="wrapper">
+              {tab.products.map((product) => (
+                <Card
+                  key={product.id}
+                  imageSrc={product.image}
+                  title={product.title}
+                  description={product.description}
+                  price={product.price}
+                  rating={5}
+                />
+              ))}
+            </div>
+          ),
+        }))}
       />
-      <Button size="large" label="Add to Cart" onClick={() => {}} />
-      <Button
-        variant="contained"
-        size="medium"
-        label="Learn More"
-        onClick={() => {}}
-      />
-      <Button label="Add to Cart" onClick={() => {}} />
       <div className="wrapper">
         <Card
           imageSrc="mock-2.png"
---
+++ src/ui/Card/Card.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Card } from '../Card';
+
+const meta: Meta<typeof Card> = {
+  title: 'UI/Card',
+  component: Card,
+  args: {
+    imageSrc: "mock-2.png",
+    title: "Example Product",
+    price: 19.99,
+    description: "This is an example product description.",
+    rating: 5,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Card>;
+
+export const Main: Story = {
+  args: {
+    imageSrc: "mock-2.png",
+    title: "Example Product",
+    price: 19.99,
+    description: "This is an example product description.",
+    rating: 5,
+  },
+};
