0661776dfe43518b6f201ef9027f640eb5b22678
mbadalian <supreme.opp@gmail.com>
2023-07-08 18:05:06 +0300

    add Section component

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,6 +2,7 @@ export { Button } from "./Button";
 export { Card } from "./Card";
 export { Tabs, Tab } from "./Tabs";
 export { DiscountItem } from "./DiscountItem";
+export { Section } from "./Section";
 
 
 export type { ButtonProps } from "./Button";
