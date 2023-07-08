170c0743a2e502d43935a827748f88ac1521a437
mbadalian <supreme.opp@gmail.com>
2023-07-08 17:12:16 +0300

    add DiscountItem

---
+++ src/ui/DiscountItem/index.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { DiscountItem } from '.';
+
+const meta: Meta<typeof DiscountItem> = {
+  title: 'UI/DiscountItem',
+  component: DiscountItem,
+  args: {
+    image: "https://uploads-ssl.webflow.com/63e857eaeaf853471d5335ff/63e8c4e6cd367817e964f756_sofa-min.png",
+    title: "Example Product",
+    price: 100,
+    description: "Explore Our Furniture & Home Furnishing Range",
+    color: "#cb9917",
+    background: "#f2e4d9",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof DiscountItem>;
+
+export const Main: Story = {
+  args: {
+    image: "https://uploads-ssl.webflow.com/63e857eaeaf853471d5335ff/63e8c4e6cd367817e964f756_sofa-min.png",
+    title: "Example Product",
+    price: 100,
+    description: "Explore Our Furniture & Home Furnishing Range",
+    color: "#cb9917",
+    background: "#f2e4d9",
+  },
+};
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,6 +1,7 @@
 export { Button } from "./Button";
 export { Card } from "./Card";
 export { Tabs, Tab } from "./Tabs";
+export { DiscountItem } from "./DiscountItem";
 
 
 export type { ButtonProps } from "./Button";
